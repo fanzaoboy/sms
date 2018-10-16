3f6faae673e494e86e7567e1fec7e2c521fa6c5c
foton <foton@01JRFWB675>
2018-10-16 16:55:21 +0800

    add timezone

--- src/com/sms/Utils/DBUtil.java
+++ src/com/sms/Utils/DBUtil.java
@@ -4,23 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBUtil {
-	
-	private static String url = "jdbc:mysql://localhost:3306/app";
+
+	private static String url = "jdbc:mysql://localhost:3306/app?serverTimezone=GMT";
 	private static String user = "root";
 	private static String password = "root";
-	
+
 	public static Connection getConn() {
 		Connection conn = null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			System.out.println("MySQL Driver Load Success!");
-			conn = DriverManager.getConnection(url,user,password);
+			conn = DriverManager.getConnection(url, user, password);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return conn;
-		
+
 	}
 }
