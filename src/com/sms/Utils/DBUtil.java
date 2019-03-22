0475c5ddb52c8f4c9b09776c983e58bbf189d353
foton <foton@01JRFWB675>
2019-03-22 14:58:28 +0800

    优化DBUtil

--- src/com/sms/Utils/DBUtil.java
+++ src/com/sms/Utils/DBUtil.java
@@ -1,33 +1,55 @@
 package com.sms.Utils;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
 
 public class DBUtil {
 
-	private static String url = "jdbc:mysql://localhost:3306/app?serverTimezone=GMT";
-	private static String user = "root";
-	private static String password = "root";
+	private static String db_driver = null;
+	private static String db_url = null;
+	private static String db_username = null;
+	private static String db_password = null;
+	private static Logger logger = Logger.getLogger(DBUtil.class);
 
-	//获取Connection对象
-	public static Connection getConn() {
-		Connection conn = null;
+	static {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("MySQL Driver Load Success!");
-			conn = DriverManager.getConnection(url, user, password);
+			// 读取db.properties文件中的数据库连接信息
+			InputStream in = DBUtil.class.getClassLoader().getResourceAsStream("db.properties");
+			Properties prop = new Properties();
+			prop.load(in);
+			db_driver = prop.getProperty("driver");
+			db_url = prop.getProperty("url");
+			db_username = prop.getProperty("username");
+			db_password = prop.getProperty("password");
+			
+			Class.forName(db_driver);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	// 获取Connection对象
+	public static Connection getConn() {
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection(db_url, db_username, db_password);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		logger.debug("MySQL驱动加载");
 		return conn;
 	}
-	
-	//有结果集的关闭
-	public static void close(Connection conn,PreparedStatement ps,ResultSet res) {
-		if(conn != null && ps != null && res != null) {
+
+	// 有结果集的关闭
+	public static void close(Connection conn, PreparedStatement ps, ResultSet res) {
+		if (conn != null && ps != null && res != null) {
 			try {
 				conn.close();
 				ps.close();
@@ -35,20 +57,20 @@ public class DBUtil {
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
-	
-	//没有结果集的关闭
-	public static void close(Connection conn,PreparedStatement ps) {
-		if(conn != null && ps != null) {
+
+	// 没有结果集的关闭
+	public static void close(Connection conn, PreparedStatement ps) {
+		if (conn != null && ps != null) {
 			try {
 				conn.close();
 				ps.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-			
+
 		}
 	}
 }
